ad006457c6ea214d7ca4da24cb2d6be81dc6fe5a
mterrell <mitchs.channel@gmail.com>
2022-04-16 16:17:53 -0500

    trying to get theme to build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,11 +44,16 @@ extensions = [
     "myst_parser",
     "sphinx_copybutton",
     "sphinx_design",
-    "sphinx_inline_tabs",
+    "sphinx_inline_tabs"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+#
+# -- Options for TODOs -------------------------------------------------------
+#
+todo_include_todos = True
 
 #
 # -- Options for extlinks ----------------------------------------------------
@@ -76,7 +81,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
