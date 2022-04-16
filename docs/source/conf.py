6db78b8dc3bc9a2811538e420621d1e3489a74ca
mterrell <mitchs.channel@gmail.com>
2022-04-16 15:37:53 -0500

    theme change attempt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,7 @@ release = '1.0_beta'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['furo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +46,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
