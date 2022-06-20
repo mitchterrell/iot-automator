3e7aa2583678e2aadffe427f8a8b4f16aa1955e5
mterrell <mitchs.channel@gmail.com>
2022-06-20 12:04:11 -0500

    Added iframe for figma stuff into documentation, checking the auto build process

--- docs/conf.py
+++ docs/conf.py
@@ -1,35 +1,18 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'iot-automator'
-copyright = '2022, Mitchell Terrell'
-author = 'Mitchell Terrell'
-
-# The full version, including alpha/beta/rc tags
-release = '1.0_beta'
+project     = 'iot-automator'
+copyright   = '2022, Mitchell Terrell'
+author      = 'Mitchell Terrell'
+release     = '1.0_beta'
 
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Sphinx extensions to include in the project
+
 extensions = [
     # Sphinx's own extensions
     "sphinx.ext.autodoc",
@@ -69,36 +52,24 @@ intersphinx_mapping = {
     "sphinx": ("https://www.sphinx-doc.org/en/master", None),
 }
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# If we add folders that we want excluded we add the realitive path here
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# Theme selection 
 html_theme = "furo"
 
+# This path contains a folder for statically linked files to the webpage (i.e. css + js files)
+html_static_path    = ['_static']
 
+html_logo           = "_static/colored_logo_w_name.svg"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_title          = "iot-automator"
 
-html_logo = "_static/colored_logo_w_name.svg"
-
-html_title= "iot-automator"
-
-html_favicon = '_static/colored_logo.svg'
+html_favicon        = '_static/colored_logo.svg'
 
 html_theme_options = {
     "sidebar_hide_name": True,
 }
-
-
-
-
--- docs/topic_overview/system_overview.rst
+++ docs/topic_overview/system_overview.rst
@@ -1,4 +1,11 @@
 System Overview
 ===============
 
-TODO
+.. raw:: html
+
+	<iframe 
+		style="border: 1px solid rgba(0, 0, 0, 0.1);"
+		width="800" 
+		height="450" 
+		src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FOsHe8qITeqduFqKDeS2yYO%2FDense-IoT%3Fpage-id%3D0%253A1%26node-id%3D167%253A654%26viewport%3D2403%252C-158%252C0.22%26scaling%3Dscale-down%26starting-point-node-id%3D167%253A654%26show-proto-sidebar%3D1" allowfullscreen>
+	</iframe>
