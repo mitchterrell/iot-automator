43baf72c13e7ecd2b37d8828fe95e1ba01d557d8
mterrell <mitchs.channel@gmail.com>
2022-07-01 18:38:16 -0500

    Added a linting file, vscode workspace requirments AND added steps for a github action on linting

--- README.rst
+++ README.rst
@@ -1,3 +1,5 @@
+[![GitHub Super-Linter](https://github.com/mitchterrell/iot-automator/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
+
 TOODO add image
 
 TODO add python version, pip support, etc. here
---
+++ src/test_files/test_lint_docs.py
@@ -0,0 +1,27 @@
+"""
+    This is a module docstring!
+"""
+
+
+def hey(mitch, terrell2) -> None:
+    """
+    hey _summary_
+
+    :param mitch: _description_
+    :type mitch: _type_
+    :param terrell2: _description_
+    :type terrell2: _type_
+    """
+    print(f"Hey! {mitch} {terrell2}")
+
+
+def hello_there(mitch, terrell):
+    """
+    hello_there _summary_
+
+    :param mitch: _description_
+    :type mitch: _type_
+    :param terrell: _description_
+    :type terrell: _type_
+    """
+    print("oooooo" + mitch + terrell)
