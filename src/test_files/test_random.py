f07d46c70f12ad399ce80e162b0a6b539fa2dd83
brow5190 <brow5190@umn.edu>
2022-07-07 20:52:21 -0700

    Fixed unit tests to work correctly and apss

--- src/test_files/test_random.py
+++ src/test_files/test_random.py
@@ -5,5 +5,5 @@ class test_random(unittest.TestCase):
     def test_assertTrue(self):
         self.assertEqual("Truman", "Truman")
     def test_assertFalse(self):
-        self.assertEqual("Truman", "Mitch")
+        self.assertNotEqual("Truman", "Mitch")
 
